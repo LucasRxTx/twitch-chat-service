12c4ff70e9d5fc963351dbc8d505e20ed77bf929
Lucas Scott <ishrocker@host-78-76-49-72.homerun.telia.com>
2020-01-20 13:04:43 +0100

    remove code-test

--- infrastructure/servers.tf
+++ infrastructure/servers.tf
@@ -1,6 +1,6 @@
 resource "digitalocean_droplet" "web" {
   image  = "ubuntu-18-04-x64"
-  name   = "strafe-code-test"
+  name   = "twitch-chat-service"
   region = "lon1"
   size   = "s-1vcpu-1gb"
   ssh_keys = [digitalocean_ssh_key.default.fingerprint]
